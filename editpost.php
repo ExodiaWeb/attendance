b477578ed42f1ab21b366dc6d7f52c115aa582be
Exodia <96380231+ExodiaWeb@users.noreply.github.com>
2021-12-26 13:42:24 +0200

    Initial commit

---
+++ editpost.php
@@ -0,0 +1,24 @@
+<?php
+    REQUIRE_ONCE 'db/conn.php';
+
+if (isset($_POST['submit'])) {
+    // extract values from the $POST array
+    $id = $_POST["id"];
+    $fname = $_POST["firstname"];
+    $lname = $_POST["lastname"];
+    $dob = $_POST["dob"];
+    $email = $_POST["email"];
+    $contact = $_POST["phone"];
+    $specialty = $_POST["specialty"];
+
+
+    $result = $crud -> editAttendee($id, $fname, $lname, $dob, $email, $contact, $specialty);
+
+    if($result){
+        header("Location: viewrecords.php");
+    } else{
+        include 'includes/errormessage.php';
+    }
+}else {
+    include 'includes/errormessage.php';
+}
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+
+    <link rel="stylesheet" href="css/site.css">
+
+    <title>Attendance - <?php echo $title; ?></title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container container-fluid">
+    <a class="navbar-brand" href="index.php">IT Conference</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        <a class="nav-link" href="viewrecords.php">View Attendees</a>
+
+      </div>
+    </div>
+  </div>
+</nav>
+<div class='container my-5'>
