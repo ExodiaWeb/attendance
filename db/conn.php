d3a623b949b07af4fb66060d6cfb66590e8d7331
Exodia <96380231+ExodiaWeb@users.noreply.github.com>
2021-12-27 12:07:59 +0200

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,10 +1,12 @@
 <?php 
 
-$host = 'localhost';
-$db = 'olivdkff_demo';
-$user = 'olivdkff_login';
-$pass = 'SobaGPozwzhf';
+$host = '127.0.0.1';
+$db = 'attendance';
+$user = 'root';
+$pass = '';
 $charset = 'utf8mb4';
+
+
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
